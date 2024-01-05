2ac552007da1c5e7a744842fb6e88732c63adf9a
Anastasia Romanyuk <vnstyv@gmail.com>
2024-01-05 22:51:36 +0200

    ed4

--- src/main.js
+++ src/main.js
@@ -10,10 +10,10 @@ const pageLoader = document.querySelector(".loader");
 const pageGallery = document.querySelector(".gallery");
 
 const searchParamsDefaults = {
-  key: '34228101-50b55348103eeb6dd10b59f8d',
-  q: 'cat',
-  image_type: 'photo',
-  orientation: 'horizontal',
+  key: "34228101-50b55348103eeb6dd10b59f8d",
+  q: "cat",
+  image_type: "photo",
+  orientation: "horizontal",
   safesearch: true,
 };
 
@@ -25,3 +25,61 @@ const lightbox = new SimpleLightbox(".gallery a", {
     enableKeyboard: true,
     docClose: true
     });
+
+function searchImg(params) {
+    return fetch(`https://pixabay.com/api/?${params}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(({ hits }) => {
+            if (hits.length > 0) {
+                const renderImg = hits.reduce((html, hit) => {
+                    return (
+                        html +
+                        `<li class="gallery-item">
+                        <a href=${hit.largeImageURL}>
+                        <img class="gallery-img" src=${hit.webformatURL} alt=${hit.tags}>
+                        </a>
+                        <div class="gallery-text-box">
+                        <p>Likes: <span class="text-value">${hit.likes}</span></p>
+                        <p>Views: <span class="text-value">${hit.views}</span></p>
+                        <p>Comments: <span class="text-value">${hit.comments}</span></p>
+                        <p>Downloads: <span class="text-value">${hit.downloads}</span></p>
+                        </div>
+                        </li>`
+                
+                    );
+                }, '');
+
+                pageGallery.innerHTML = renderImg;
+
+                lightbox.refresh();
+
+            } else {
+                iziToast.error({
+                    position: 'topRight',
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                });
+            }
+        })
+    
+        .catch(error => {
+            console.log(error.message);
+        })
+        .finally(() => {
+            pageLoader.style.display = 'none';
+        });
+}
+
+pageForm.addEventListener("submit", event => {
+    event.preventDefault();
+    pageGallery.innerHTML = '';
+    pageLoader.style.display = 'block';
+    searchParamsDefaults.q = event.target.elements.search.value.trim();
+    const searchParams = new URLSearchParams(searchParamsDefaults);
+    searchImg(searchParams.toString());
+    event.currentTarget.reset();
+});
